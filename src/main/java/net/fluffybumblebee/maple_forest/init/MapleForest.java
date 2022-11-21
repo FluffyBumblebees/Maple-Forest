8c30072197c53fe8ea3f4429ad6102c3b906072e
Fluffy Bumblebee <lromanic123alt@gmail.com>
2022-11-21 14:35:20 +1100

    Config

--- src/main/java/net/fluffybumblebee/maple_forest/init/MapleForest.java
+++ src/main/java/net/fluffybumblebee/maple_forest/init/MapleForest.java
@@ -2,11 +2,14 @@ package net.fluffybumblebee.maple_forest.init;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fluffybumblebee.maple_forest.config.MFConfig;
 import net.fluffybumblebee.maple_forest.world.placer.MFPlacerTypes;
 import net.fluffybumblebee.maple_forest.entity.MFEntities;
 import net.fluffybumblebee.maple_forest.item.MFItems;
 import net.fluffybumblebee.maple_forest.item.group.MFItemGroup;
 import net.fluffybumblebee.maple_forest.world.gen.MFWorldGen;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 
@@ -14,8 +17,10 @@ public class MapleForest implements ModInitializer {
     public static final String NAMESPACE = "maple_forest";
     private static Boolean initialized = false;
     private static final ArrayList<Runnable> runnables = new ArrayList<>(1);
+    public static final Logger LOGGER = LoggerFactory.getLogger("Fastload");
     @Override
     public void onInitialize() {
+        MFConfig.loadClass();
         MFRegistry.addToRegistry();
         MFItems.addToRegistry();
         MFEntities.addToRegistry();
