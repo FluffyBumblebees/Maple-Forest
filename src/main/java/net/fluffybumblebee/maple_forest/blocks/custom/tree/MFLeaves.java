53097e992691cce77de46f19534c9fa283210989
Fluffy Bumblebee <lromanic123alt@gmail.com>
2022-12-07 23:40:56 +1100

    update leaves

--- src/main/java/net/fluffybumblebee/maple_forest/blocks/custom/tree/MFLeaves.java
+++ src/main/java/net/fluffybumblebee/maple_forest/blocks/custom/tree/MFLeaves.java
@@ -3,9 +3,18 @@ package net.fluffybumblebee.maple_forest.blocks.custom.tree;
 import net.fluffybumblebee.maple_forest.util.block.MFBlockUtil;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.LeavesBlock;
+import net.minecraft.sound.BlockSoundGroup;
 
 public class MFLeaves extends LeavesBlock {
     public MFLeaves() {
-        super(Settings.copy(Blocks.OAK_LEAVES).nonOpaque().allowsSpawning(MFBlockUtil::canSpawnOnLeaves).suffocates(MFBlockUtil::never));
+        super(Settings
+                .copy(Blocks.OAK_LEAVES)
+                .nonOpaque()
+                .ticksRandomly()
+                .strength(0.2F)
+                .sounds(BlockSoundGroup.GRASS)
+                .suffocates(MFBlockUtil::never)
+                .allowsSpawning(MFBlockUtil::canSpawnOnLeaves)
+                .blockVision(MFBlockUtil::never));
     }
 }
